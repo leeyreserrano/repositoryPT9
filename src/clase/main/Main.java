cc2fc6a6d7a79aa592056ebc57d9a09260568069
dam1 <dam1@A80533400682176>
2025-02-13 10:05:21 +0100

    4a versió

--- src/clase/main/Main.java
+++ src/clase/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("3a modificació Git");
+		System.out.println("4a modificació Git Branca2");
 	}
 
 }
